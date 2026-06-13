d8bf89d95913c57c450e759a3544ec38ab850c17
Streatlab <ruben@streatlab.com>
2026-06-14 00:01:12 +0200

    Responsive Pasada 1: activar capa global movil

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/responsive-movil.css'
 import App from './App.tsx'
 import { ThemeProvider } from './contexts/ThemeContext'
 import { IVAProvider } from './contexts/IVAContext'
